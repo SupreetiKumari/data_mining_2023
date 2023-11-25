37b9e9369ec05946be786872c5d28b641aff19ce
Goonjan Saha <77939674+sgoonjan@users.noreply.github.com>
2023-11-25 22:34:04 +0530

    Update baselines.py

--- A3/classification/baselines.py
+++ A3/classification/baselines.py
@@ -1,5 +1,5 @@
 from sklearn.dummy import DummyClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 from sklearn.linear_model import LogisticRegression
 
 
