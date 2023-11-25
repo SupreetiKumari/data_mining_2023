cfa7bbffab4b4630564c2687cfbc9e91228a4c87
Goonjan Saha <77939674+sgoonjan@users.noreply.github.com>
2023-11-25 20:04:47 +0530

    Delete A1 directory

--- A1/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++17 compress.cpp -o compress_bin
-g++ -std=c++17 decompress.cpp -o decompress_bin
--- A1/interface.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "C" ]; then
-    ./compress_bin $2 $3
-elif [ "$1" == "D" ]; then
-    ./decompress_bin $2 $3
-else
-    echo "Invalid input"
-fi
